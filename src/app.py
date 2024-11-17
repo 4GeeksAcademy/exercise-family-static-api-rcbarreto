ad4efdae4439c840ab0aba5670db989c9914c1a0
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-11-17 22:58:23 +0000

    Pronto

--- src/app.py
+++ src/app.py
@@ -36,7 +36,42 @@ def handle_hello():
     }
 
 
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+
+@app.route('/member', methods=['POST'])
+def new_member():
+
+    # this is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_all_members()
+
+    request_body = request.json
+    members.append(request_body)
+    print("Incoming request with the following body", request_body)
+    
+    return jsonify(members), 200
+
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(id)
+    
+    if member != None:
+        return jsonify(member), 200
+    return jsonify ({"msj":"not found"}), 404
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member_by_id(id):
+    member = jackson_family.delete_member(id)
+    if member != None:
+        return jsonify ({"done": True}), 200
+    return jsonify ({"msj":"not found"}), 404
+    
+    
+   
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
